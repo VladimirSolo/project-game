04591316f58b40991c8f402ada87ce5987d67f36
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-19 17:56:29 +0600

    check db connection

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ require('dotenv').config();
 require('@babel/register');
 const express = require('express');
 
+const { sequelize } = require('./db/models');
+
 const app = express();
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
+app.listen(PORT ?? 3100, async () => {
   console.log(`SERVER STATRED ON port ${PORT}`);
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to DB established');
+  } catch (error) {
+    console.log('Connection to DB not connect', error);
+  }
 });
