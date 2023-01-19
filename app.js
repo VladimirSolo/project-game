e52e8b49be0edf8f951605390c815efb5588c0bb
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-19 18:17:12 +0600

    create main page

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
-require('@babel/register');
+
 const express = require('express');
 
 const { sequelize } = require('./db/models');
+// routers
+const mainRouter = require('./routers/mainPageRoter');
 
 const app = express();
 const { PORT } = process.env;
+
+app.use('/', mainRouter);
 
 app.listen(PORT ?? 3100, async () => {
   console.log(`SERVER STATRED ON port ${PORT}`);
---
+++ views/MainPage.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+const Layout = require('./Layout');
+
+function MainPage() {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+    </Layout>
+  );
+}
+
+module.exports = MainPage;
