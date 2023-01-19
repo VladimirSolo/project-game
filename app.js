d195e4bc890066828bc7abf376fd6549045827c0
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-19 17:52:58 +0600

    start easy server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 require('dotenv').config();
 require('@babel/register');
+const express = require('express');
+
+const app = express();
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`SERVER STATRED ON port ${PORT}`);
+});
