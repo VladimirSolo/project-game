2b46dca435d6afd25d9b2e35d1faf51de5124881
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-19 18:35:35 +0600

    add background styles on main page

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -8,9 +8,12 @@ function Layout({ children }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>title</title>
+        <link rel="stylesheet" href="/style/style.css" />
       </head>
       <body>
-        {children}
+        <div className="container">
+          {children}
+        </div>
       </body>
     </html>
   );
